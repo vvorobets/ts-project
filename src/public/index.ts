575a1b3c5735026bb46795790711e5c6e1624071
vvorobets <vvorobets@gmail.com>
2018-07-20 01:36:46 +0300

    Update config with few validation options

--- src/public/index.ts
+++ src/public/index.ts
@@ -2,15 +2,9 @@ import fight from './fight/fight';
 import Fighter from './fight/fighter';
 import ImprovedFighter from './fight/improvedFighter';
 
-export default class Main {
-    constructor() {
-        // create two instances
-        const firstFighter = new Fighter();
-        const secondFighter = new ImprovedFighter();
+// create two instances
+const firstFighter = new Fighter();
+const secondFighter = new ImprovedFighter();
 
-        // call fight function
-        const app = fight(firstFighter, secondFighter, 1, 2, 3, 1, 5, 2, 4, 2, 6, 8, 3, 5, 7);
-    }
-}
-
-const start = new Main();
+// call fight function
+fight(firstFighter, secondFighter, 1, 2, 3, 1, 5, 2, 4, 2, 6, 8, 3, 5, 7);
